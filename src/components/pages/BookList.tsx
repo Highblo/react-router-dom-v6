c510b8469a555d8c8ef3745d87efa24017ca7379
siotan <nyaa.-525@outlook.jp>
2022-11-21 23:37:25 +0900

    react-intersection-observer使ってみた

--- src/components/pages/BookList.tsx
+++ src/components/pages/BookList.tsx
@@ -1,9 +1,45 @@
 import { FC, memo } from "react";
+import styled from "styled-components";
+import { useInView } from "react-intersection-observer";
+import "./BookList.css";
 
 export const BookList: FC = memo(() => {
+  const { ref, inView } = useInView({
+    rootMargin: "100px",
+    triggerOnce: false,
+  });
+
+  const boxs =  [{
+    text: "box1",
+    className: "fadeIn"
+  },
+  {
+    text: "box2",
+    className: "fadeIn2"
+  },
+  {
+    text: "box3",
+    className: "fadeIn3"
+  }];
+
   return (
-    <>
-      <h1>BookList</h1>
-    </>
+    <div>
+      <h1 style={{ textAlign: "center" }}>BOOKLIST</h1>
+      <SCenter>
+        {boxs.map((box) => (
+          <div ref={ref} className={inView ? `fade ${box.className}` : "div"}>
+            <p>{box.text}</p>
+          </div>
+        ))}
+      </SCenter>
+    </div>
   );
 });
+
+const SCenter = styled.div`
+  height: 200vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
